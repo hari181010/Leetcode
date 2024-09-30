3a652857bbc5e989f806e0035cf61d4c5649423b
hari181010 <142649450+hari181010@users.noreply.github.com>
2024-09-30 20:03:23 +0530

    Time: 0 ms (100%), Space: 40.4 MB (37.96%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] c=new int[n+1];
+        c[0]=1;
+        c[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<=i-1;j++){
+                c[i]=c[i]+c[j]*c[i-j-1];
+            }
+        }
+        return c[n];
+
+        
+    }
+}

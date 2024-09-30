6f7f516e9c00eecb7710d09724a8cb84cd62b9c7
hari181010 <142649450+hari181010@users.noreply.github.com>
2024-09-30 19:27:03 +0530

    Time: 14 ms (26.56%), Space: 45.6 MB (21.38%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        String[] ans=new String[numRows];
+        for(int i=0;i<numRows;i++){
+            ans[i]="";
+        }
+        int i=0;
+        while(i<s.length()){
+            for(int idx=0;idx<numRows && i<s.length();idx++){
+                ans[idx]+=s.charAt(i++);
+            }
+            for(int idx=numRows-2;idx>0 && i<s.length();idx--){
+                ans[idx]+=s.charAt(i++);
+            }
+        }
+        String res="";
+        for(String str:ans){
+            res+=str;
+        }
+        return res;
+        
+    }
+}
